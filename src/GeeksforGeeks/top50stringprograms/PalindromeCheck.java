00c260a7a650a59a8b6cae6b43315ded724b0b30
SWAPNIL <swapnilsg99@gmail.com>
2025-03-08 20:09:12 +0530

    palindrome code push

--- src/GeeksforGeeks/top50stringprograms/PalindromeCheck.java
+++ src/GeeksforGeeks/top50stringprograms/PalindromeCheck.java
@@ -1,5 +1,27 @@
 package GeeksforGeeks.top50stringprograms;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class PalindromeCheck {
-    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String str2=new String();
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            str2=str2+str.charAt(i);
+
+        }
+        if(str.equals(str2))
+        {
+            System.out.println("This is palindrome");
+        }
+        else
+        {
+            System.out.println("This is not palindrome");
+        }
+
+    }
+
 }
