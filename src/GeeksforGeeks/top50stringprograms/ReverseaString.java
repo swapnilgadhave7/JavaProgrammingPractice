489d951f42b14192bdbd41879d1f48f29968b3c2
SWAPNIL <swapnilsg99@gmail.com>
2025-03-08 20:21:33 +0530

    palindrome code push

---
+++ src/GeeksforGeeks/top50stringprograms/ReverseaString.java
@@ -0,0 +1,15 @@
+package GeeksforGeeks.top50stringprograms;
+import java.util.Scanner;
+
+public class ReverseaString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String str2=new String();
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            str2=str2+str.charAt(i);
+        }
+        System.out.println(str2);
+    }
+}
