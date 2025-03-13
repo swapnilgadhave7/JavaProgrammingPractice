f661746f004af24f22bb22f4aa7d47f3847bd7a5
SWAPNIL <swapnilsg99@gmail.com>
2025-03-13 22:58:58 +0530

    Maximum consecutive one’s (or zeros) in a binary array

--- src/GeeksforGeeks/top50Arrayprogram/Maximumconsecutiveone.java
+++ src/GeeksforGeeks/top50Arrayprogram/Maximumconsecutiveone.java
@@ -5,7 +5,7 @@ public class Maximumconsecutiveone {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int arr[]=new int[n];
+        int[] arr=new int[n];
         int maxocc=Integer.MIN_VALUE;
         int count=0;
         for(int i=0;i<n;i++)
